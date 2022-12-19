f4d01410d35efd0c9b86e3ca923f6b51e540892c
Igor Lemões Xavier <ilx.cancao@gmail.com>
2022-12-19 10:28:21 -0300

    Modifica settings porta

--- base/settings.py
+++ base/settings.py
@@ -101,7 +101,7 @@ DATABASES = {
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
         'HOST': os.getenv('HOSTPG'),
         'DISABLE_SERVER_SIDE_CURSORS': True,
-        'PORT': os.getenv('ALT_PORT_PG'),
+        'PORT': os.getenv('ALT_PORT_PG', '5432'),
     }
 }
 
